1cadfbf94fa897e973372a7b792bcfca6b468084
Camille <camille.janicki@gmail.com>
2017-08-27 22:07:16 -0700

    finalizing script

--- install.sh
+++ install.sh
@@ -3,21 +3,20 @@ CONFIG_REPO=$GIT_DIR/config
 USER=camille
 
 apt-get install software-properties-common
-add-apt repository "deb http://ppa.launchpad.net/ansible/ansible/ubuntu zesty main"
+add-apt-repository "ppa:ansible/ansible"
 apt-get update
 apt-get upgrade
 
 echo "Install python and ansible"
 
 apt-get install python-dev python-pip
-chown -R $USER:$USER ~/.cache
 apt-get install ansible
 pip install ansible markupsafe
 chown -R $USER:$USER ~/.ansible
 
 echo "Run ansible provisioning playbooks"
 
-ANSIBLE_NOCOWS=1 ansible-playbook -K -c local -i localhost, packages.yml -vvv
+ANSIBLE_NOCOWS=1 ansible-playbook -K -c local -i localhost, packages.yml 
 
 echo "Install Sublime Text"
 
@@ -27,10 +26,6 @@ echo "Install warp drive"
 
 curl -L http://github.com/mfaerevaag/wd/raw/master/install.sh | sh
 
-echo "Install fonts"
-
-bash ./fonts/install.sh
-
 echo "Install nvm"
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
@@ -38,6 +33,6 @@ chown -R $USER:$USER ~/.nvm
 
 echo "Setup symlinks"
 
-ln -sf $CONFIG_REPO/config/ .xbindkeysrc ~/.xbindkeysrc
+ln -sf $CONFIG_REPO/.xbindkeysrc ~/.xbindkeysrc
 
-# Install libreoffice english dictionary
+echo " Install libreoffice english dictionary"
